472f0c3fc5dc7d5398157c721fdccbc6e36d9b00
beomjungil <gil@beomjun.kr>
2022-08-17 23:16:16 +0900

    Remove useless property icon

--- FontAwesomeComposeLib/src/main/java/com/guru/fontawesomecomposelib/FaIconType.kt
+++ FontAwesomeComposeLib/src/main/java/com/guru/fontawesomecomposelib/FaIconType.kt
@@ -2,11 +2,11 @@ package com.guru.fontawesomecomposelib
 
 
 sealed class FaIconType(val src: Int) {
-    class SolidIcon(private val icon: Int) : FaIconType(icon)
+    class SolidIcon(icon: Int) : FaIconType(icon)
 
-    class RegularIcon(private val icon: Int) : FaIconType(icon)
+    class RegularIcon(icon: Int) : FaIconType(icon)
 
-    class BrandIcon(private val icon: Int) : FaIconType(icon)
+    class BrandIcon(icon: Int) : FaIconType(icon)
 }
 
 
